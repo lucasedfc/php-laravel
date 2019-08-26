d29b9b64a1c872077ed20519976ae77fe70d913e
Lucas <lucasedfc@gmail.com>
2019-08-26 00:12:45 -0300

    Init Project

---
+++ probando-laravel/app/Http/routes.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello Zoquete';
+});
+
+Route::get('/contacto-test', function () {
+    return view('contacto');
+});
+
+Route::post('/hello', function () {
+    return 'Hello Zoquete por POST';
+});
+
+/*
+Route::match(['get', 'post'], 'contacto', function() {
+    return view('contacto');
+});
+*/
+Route::any('test-methods', function() {
+    return 'This works in all http methods';
+});
+
+//Parametros
+Route::get('/contacto/{name?}/{age?}', function ($name = "Lucas",
+$age = 29) {
+    return view('contacto', array(
+        "name" => $name,
+        "age" => $age
+    ));
+})->where([ //Validacion de formato
+    'name' => '[A-Za-z]+',
+    'age' => '[0-9]+'
+]);
