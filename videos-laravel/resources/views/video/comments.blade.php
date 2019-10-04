bb391f89dfb0e3222c62922c6abb7ca6e7bd4e92
Lucas Heim <lheim@asesconsulting.com>
2019-10-03 21:28:23 -0300

    Delete comment

--- videos-laravel/resources/views/video/comments.blade.php
+++ videos-laravel/resources/views/video/comments.blade.php
@@ -41,7 +41,38 @@
                     <div class="panel-body">
                         {{$comment->body}}
 
+                    @if( Auth::check() && (Auth::user()->id == $comment->id || Auth::user()->id == $video->user->id))
+                    <div class="pull-right clearfix">
+                        <a href="#comment-modal{{$comment->id}}" role="button" class="btn btn-sm btn-danger" data-toggle="modal">Delete</a>
+                        
+                        <!-- Modal / Ventana / Overlay en HTML -->
+                        <div id="comment-modal{{$comment->id}}" class="modal fade">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title">¿Are you sure?</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>¿Do you want to delete this comment?</p>
+                                            <p class="text-warning"><small>{{$comment->body}}</small></p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-danger">Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+  
+
+                    @endif
+
                     </div>
+
+                   
                 </div>
             </div>
             @endforeach
